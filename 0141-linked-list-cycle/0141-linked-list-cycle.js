590224053b51b97b132854270697035756d0b1c0
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2025-08-23 10:31:07 +0530

    Time: 49 ms (75.09%), Space: 57.9 MB (94.02%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let left=head
+    let right =head
+
+    while(right && right.next)
+    {
+        
+        right =right.next.next
+        left=left.next
+
+        if(left===right)
+        return true
+    }
+    return false
+};
