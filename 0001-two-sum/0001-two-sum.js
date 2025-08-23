dc15cbe9caddc732fc3cfdd386acfab782e8f0cf
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2025-08-23 10:32:45 +0530

    Time: 0 ms (100%), Space: 54.5 MB (62.05%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+      let obj={};
+  for(let i=0;i<nums.length;i++)
+  {
+    if(obj[target-nums[i]]>=0)
+    {
+      return[obj[target-nums[i]],i]
+    }
+    else
+    {
+      obj[nums[i]]=i;
+    }
+  }
+  
+};
