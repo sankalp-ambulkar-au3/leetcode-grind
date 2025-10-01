980904db27aa23c73c05ef2c8e807bc5f89ee0a7
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2025-10-01 09:00:41 +0530

    Time: 2 ms (26.75%), Space: 58.3 MB (26.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,16 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    let left =0;
+var removeDuplicates = function (nums) {
+    // let left =0;
 
-    for(let right=1;right<nums.length;right++)
-    {
-        if(nums[left]!==nums[right])
-        {
-            ++left
-            nums[left]=nums[right]
+    // for(let right=1;right<nums.length;right++)
+    // {
+    //     if(nums[left]!==nums[right])
+    //     {
+    //         ++left
+    //         nums[left]=nums[right]
+    //     }
+    // }
+    // return left+1
+
+    let left = 0;
+    let right = 1;
+    while (right < nums.length) {
+        if (nums[left] !== nums[right]) {
+            left++;
+            nums[left] = nums[right];
         }
+        right++;
     }
-    return left+1
+    return left + 1;
 };
