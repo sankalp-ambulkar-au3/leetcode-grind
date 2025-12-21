ef98750f2e78ccfbff2fb472132a9f74c914c216
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2025-12-21 12:02:47 +0530

    Time: 6 ms (68.31%), Space: 59.6 MB (29.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,26 +1,25 @@
 /**
- * @param {string} 
+ * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let left=0;
-    let right=0;
-    let max=0
-    let counter =new Map()
+    let left=0
+    let right =0
+    let count = new Map()
+    let maxSoFar=0
     while(right<s.length)
     {
-        if(counter.has(s[right]))
+        if(count.has(s[right]))
         {
-            while(counter.has(s[right]))
+            while(count.has(s[right]))
             {
-                counter.delete(s[left])
-                left++
+            count.delete(s[left])
+            left++
             }
         }
-        counter.set(s[right],1)
-        max=Math.max(max,right-left+1)
+        count.set(s[right],1)
+        maxSoFar=Math.max(maxSoFar,right-left+1)
         right++
     }
-    return max
-
+    return maxSoFar
 };
