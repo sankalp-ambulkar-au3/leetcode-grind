927bdce4731c8e7ee79053233f69e4b01542ada9
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2025-12-24 19:32:04 +0530

    Time: 4 ms (41.64%), Space: 60.5 MB (34.35%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let res=[]
+    function backtrack(start,path,remainder)
+    {
+        if(remainder===0)
+        {
+            res.push([...path])
+            return 
+        }
+        if(remainder<0)
+        return 
+        for(let i=start;i<candidates.length;i++)
+        {
+            path.push(candidates[i])
+            backtrack(i,path,remainder-candidates[i])
+            path.pop()
+        }
+    }
+    backtrack(0,[],target)
+    return res
+};
