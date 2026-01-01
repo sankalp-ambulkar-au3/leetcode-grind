57276e3166174a49a5d5cdb04628d4711038cb70
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-01-01 14:14:35 +0530

    Time: 0 ms (100%), Space: 53.8 MB (85.07%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = Array.from({ length: m }, () => Array(n).fill(0));
+
+  dp[0][0] = obstacleGrid[0][0] === 1 ? 0 : 1;
+
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = obstacleGrid[i][0] === 1 ? 0 : dp[i - 1][0];
+  }
+
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = obstacleGrid[0][j] === 1 ? 0 : dp[0][j - 1];
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) dp[i][j] = 0;
+      else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
