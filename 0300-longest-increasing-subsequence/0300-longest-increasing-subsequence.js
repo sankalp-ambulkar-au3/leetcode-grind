cd32d0047ae71afdc30303254e2817db66d69e15
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-01-14 18:05:03 +0530

    Time: 85 ms (47.68%), Space: 54.5 MB (90.4%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const dp=Array(nums.length).fill(1)
+    for(let i=0;i<nums.length;i++)
+    {
+        for(let j=0;j<i;j++)
+        {
+            if(nums[j]<nums[i])
+            dp[i]=Math.max(dp[i],dp[j]+1)
+        }
+    }
+    return Math.max(...dp)
+};
