4c0ef885b98b5a9af6274742b64771b8a7cd03de
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-02-06 22:29:18 +0530

    Time: 7 ms (88.08%), Space: 59.4 MB (18.58%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+     let left = 1;
+  let right = Math.max(...piles);
+
+  function canFinish(k) {
+    let hours = 0;
+    for (let pile of piles) {
+      hours += Math.ceil(pile / k);
+    }
+    return hours <= h;
+  }
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (canFinish(mid)) {
+      right=mid;
+    } else left = mid + 1;
+  }
+  return left; 
+};
