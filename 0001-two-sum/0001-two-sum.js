83f2a8608c5626526be14c0e364056fefc8c9295
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-26 21:06:32 +0530

    Time: 0 ms (100%), Space: 55.6 MB (34.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,29 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-//       let obj={};
-//   for(let i=0;i<nums.length;i++)
-//   {
-//     if(obj[target-nums[i]]>=0)
-//     {
-//       return[obj[target-nums[i]],i]
-//     }
-//     else
-//     {
-//       obj[nums[i]]=i;
-//     }
-        const numMap=new Map();
-
+    let map = new Map()
     for(let i=0;i<nums.length;i++)
     {
-        const complimentNumber = target-nums[i];
-
-        if(numMap.has(complimentNumber))
-        {
-        return [numMap.get(complimentNumber),i]
-        }
-
-        numMap.set(nums[i],i)
+        let complimentNumber = target-nums[i]
+        if(map.has(complimentNumber))
+        return [i,map.get(complimentNumber)]
+        else
+        map.set(nums[i],i)
     }
   }
   
