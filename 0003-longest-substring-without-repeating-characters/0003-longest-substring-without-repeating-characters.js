939d29ca41f7be0de00c3ebbe48aea53853e3752
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-27 22:00:03 +0530

    Time: 8 ms (46.2%), Space: 58.9 MB (47.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let left=0
-    let right =0
-    let count = new Map()
+    let left=0;
+    let right=0;
     let maxSoFar=0
+    let count =new Map()
     while(right<s.length)
     {
         if(count.has(s[right]))
         {
             while(count.has(s[right]))
             {
-            count.delete(s[left])
-            left++
+                count.delete(s[left])
+                left++
             }
         }
         count.set(s[right],1)
-        maxSoFar=Math.max(maxSoFar,right-left+1)
+        maxSoFar=Math.max(count.size,maxSoFar)
         right++
     }
     return maxSoFar
