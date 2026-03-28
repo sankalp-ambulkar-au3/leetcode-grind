a1b4e0c9f9fe17353d41760b5f881f5921f3ba49
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-28 14:46:52 +0530

    Time: 19 ms (55.96%), Space: 57.2 MB (37.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,21 +4,23 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-  let left = 0;
-  let res = 0;
-  let count = {};
-  let maxFrequency = 0;
-
-  for (let right = 0; right < s.length; right++) {
-    count[s[right]] = (count[s[right]] || 0) + 1;
-    maxFrequency = Math.max(maxFrequency, count[s[right]]);
-    while (right - left + 1 - maxFrequency > k) {
-      count[s[left]]--;
-      left++;
-    }
-
-    res = Math.max(res, right - left + 1);
-  }
-
-  return res;
+let left =0
+let right =0;
+let maxFrequency=0
+let count ={}
+let maxSoFar=0
+while(right<s.length)
+{
+    count[s[right]]=(count[s[right]] || 0)+1
+    maxFrequency=Math.max(maxFrequency,count[s[right]])
+   
+        while( right-left+1-maxFrequency>k)
+        {
+            count[s[left]]--
+            left++
+        }
+    maxSoFar=Math.max(maxSoFar, right-left+1)
+    right++
+}
+ return maxSoFar
 };
