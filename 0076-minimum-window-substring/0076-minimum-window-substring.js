d8ee93730dd2a3eda751dcbddf3b63135dc26c9b
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-29 14:23:45 +0530

    Time: 26 ms (51.81%), Space: 55.5 MB (94.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -4,35 +4,38 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-  if (t.length > s.length) return '';
-  let need = new Map();
-  for (let elem of t) {
-    need.set(elem, (need.get(elem) || 0) + 1);
-  }
+  if (t.length > s.length) return "";
   let left = 0;
+  let right = 0;
   let window = new Map();
+  let need = new Map();
   let found = 0;
-  let minLen = Infinity;
-  let startIndex=0
-  for (let right = 0; right < s.length; right++) {
-    c = s[right];
-    window.set(c, (window.get(c) || 0) + 1);
-    if (need.get(c)) {
-      if (window.get(c) === need.get(c)) found++;
-    }
-    while (found === need.size) {
-      if(right-left+1 <minLen)
-      {
-        minLen=right-left+1
-        startIndex=left
-      }
-      d = s[left];
-      left++;
-      if (need.get(d)) {
-        if (window.get(d) === need.get(d)) found--;
-        window.set(d, window.get(d) - 1);
-      }
-    }
+  let startIndex = 0;
+  let minimumWindow = Infinity;
+  for (let i = 0; i < t.length; i++) {
+    need.set(t[i], (need.get(t[i]) || 0) + 1);
   }
-   return minLen === Infinity ? "" : s.substring(startIndex, startIndex + minLen);
+  while (right < s.length) {
+    let d = s[right];
+    window.set(d, (window.get(d) || 0) + 1);
+    if (need.get(d)) {
+      if (need.get(d) === window.get(d)) found++;
+    }
+    while (need.size == found) {
+      if (right - left + 1 < minimumWindow) {
+        minimumWindow = right - left + 1;
+        startIndex = left;
+      }
+      let c = s[left];
+      left++;
+      if (need.get(c)) {
+        if (window.get(c) === need.get(c)) found--;
+        window.set(c, window.get(c) - 1);
+      }
+    }
+    right++;
+  }
+  return minimumWindow === Infinity
+    ? ""
+    : s.substring(startIndex, startIndex + minimumWindow);
 };
