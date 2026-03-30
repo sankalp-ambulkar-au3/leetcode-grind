b3238a1360e961cafc77edadf2dd60d7059ce3cc
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-30 21:25:19 +0530

    Time: 82 ms (5.26%), Space: 68.8 MB (6.15%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -1,13 +1,16 @@
 var maxArea = function (height) {
-  let maxWater = 0;
-  let left = 0;
-  let right = height.length - 1;
-  let localWater = 0;
-  while (left < right) {
-    localWater = Math.min(height[left], height[right]) * (right - left);
-    maxWater = Math.max(localWater, maxWater);
-    if (height[left] < height[right]) left++;
-    else right--;
-  }
-  return maxWater;
+let maxWater=0
+let maxSoFar=0
+let left=0
+let right=height.length-1
+while(left<right)
+{
+  let localWater=Math.min(height[left],height[right])*[right-left];
+  maxWater=Math.max(localWater,maxWater)
+  if(height[left]<height[right])
+    left++
+  else
+    right--
+}
+return maxWater
 };
