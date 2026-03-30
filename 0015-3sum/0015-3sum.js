65e1a9b5887bad4204814b70e367b126f472df43
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-30 21:08:29 +0530

    Time: 31 ms (87.37%), Space: 73 MB (32.01%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -9,24 +9,32 @@
 //Push these three elements in results array
 //Remove duplicates by moving pointers till you have same left and right
 var threeSum = function (nums) {
-  nums.sort((a, b) => a - b);
-  let res = [];
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-    if (nums[i]>0) break;
-    let left = i + 1;
-    let right = nums.length - 1;
-    while (left < right) {
-      let sum = nums[i] + nums[right] + nums[left];
-      if (sum === 0) {
-        res.push([nums[i], nums[left], nums[right]]);
-        left++;
-        right--;
-        while (left < right && nums[left] === nums[left - 1]) left++;
-        while (left < right && nums[right] === nums[right + 1]) right--;
-      } else if (sum < 0) left++;
-      else right--;
+  let res =[]
+  nums.sort((a,b)=>a-b)
+  for(let i=0;i<nums.length-2;i++)
+  {
+    if(i>0 && nums[i-1]===nums[i])
+      continue;
+    if(nums[i]>0)
+      break;
+    let left=i+1
+    let right=nums.length-1
+    while(left<right)
+    {
+      let sum =nums[left]+nums[right]+nums[i]
+      if(sum===0)
+      {
+        res.push([nums[i],nums[left],nums[right]])
+        left++
+        right--
+        while(left<right && nums[left]===nums[left-1])left++
+        while(left<right && nums[right]===nums[right+1])right--
+      }
+      else if(sum<0)
+        left++
+      else
+        right--
     }
   }
-  return res;
-};
+  return res
+}
