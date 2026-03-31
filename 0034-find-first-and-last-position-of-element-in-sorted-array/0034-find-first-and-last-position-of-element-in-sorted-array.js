7e83856f9934bf2893767eb0182ca912fad9653c
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-03-31 21:40:48 +0530

    Time: 0 ms (100%), Space: 54.5 MB (78.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,29 +4,23 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-
-    function binarySearch(isLeftBias) {
-        let left = 0
-        let right = nums.length-1
-        let idx = -1
-        while (left <= right) {
-            let mid = Math.floor((left + right) / 2)
-            if (nums[mid] < target)
-                left = mid + 1
-            else if (nums[mid] > target)
-                right = mid - 1
-            else {
-                idx = mid
-                if (isLeftBias)
-                    right = mid - 1
-                else
-                    left = mid + 1
-            }
-
-        }
-        return idx
+  function binarySearch(isLeftBiased) {
+    let left = 0;
+    let right = nums.length - 1;
+    let res = -1;
+    while (left <= right) {
+      let mid = Math.floor(left + (right - left) / 2);
+      if (target > nums[mid]) left = mid + 1;
+      else if (target < nums[mid]) right = mid - 1;
+      else {
+        res = mid;
+        if (isLeftBiased) right = mid - 1;
+        else left = mid + 1;
+      }
     }
-    let left = binarySearch(true)
-    let right = binarySearch(false)
-    return [left, right]
+    return res;
+  }
+  let leftPosition = binarySearch(true);
+  let rightPosition = binarySearch(false);
+  return [leftPosition, rightPosition];
 };
