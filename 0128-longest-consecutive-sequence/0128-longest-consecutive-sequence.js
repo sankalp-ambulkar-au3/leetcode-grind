7546664a98bef0bb52157ee89300dd2aaf80cf01
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-04-12 11:27:21 +0530

    Time: 36 ms (78.16%), Space: 66.3 MB (98.29%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -2,26 +2,45 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var longestConsecutive = function(nums) {
+//     if(nums.length==0)
+//     return 0
+//   let dictionary = new Set();
+//   let maxSofar = 1;
+//   for (let num of nums) {
+//     if (dictionary.has(num)) continue;
+//     dictionary.add(num, num);
+//   }
+//   for (let num of dictionary.keys()) {
+//     let count = 1;
+
+//     if (dictionary.has(num - 1)) continue;
+//     let x = num;
+//     while (dictionary.has(x + 1)) {
+//       count++;
+//       x++;
+//     }
+//     maxSofar = Math.max(maxSofar, count);
+//   }
+
+//   return maxSofar;
+// };
 var longestConsecutive = function(nums) {
     if(nums.length==0)
     return 0
-  let dictionary = new Set();
-  let maxSofar = 1;
+  nums.sort((a, b) => a - b);
+  let lastSmallest = Infinity;
+  let maxSoFar = 1;
+  let count = 0;
   for (let num of nums) {
-    if (dictionary.has(num)) continue;
-    dictionary.add(num, num);
-  }
-  for (let num of dictionary.keys()) {
-    let count = 1;
-
-    if (dictionary.has(num - 1)) continue;
-    let x = num;
-    while (dictionary.has(x + 1)) {
+    if (num - 1 == lastSmallest) {
       count++;
-      x++;
+      lastSmallest = num;
+    } else if (num !== lastSmallest) {
+      count = 1;
+      lastSmallest = num;
     }
-    maxSofar = Math.max(maxSofar, count);
+    maxSoFar = Math.max(count, maxSoFar);
   }
-
-  return maxSofar;
-};
+  return maxSoFar
+}
