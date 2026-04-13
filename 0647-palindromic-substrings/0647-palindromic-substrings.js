3886ea7fc3b1a38f237f59d87865dce22e94fcef
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-04-13 18:33:41 +0530

    Time: 17 ms (37.8%), Space: 56.5 MB (41.09%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let count =0
+  function expand(left, right) {
+    while (left >= 0 && right < s.length && s[left] == s[right]) {
+        count++
+      left--;
+      right++;
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    expand(i, i);
+    expand(i, i + 1);
+  }
+  return count;
+};
