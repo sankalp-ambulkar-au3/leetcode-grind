986fc77188c9379bf3a2b1f8c8916524592a544c
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-04-13 20:00:17 +0530

    Time: 7 ms (64.8%), Space: 59.7 MB (24.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -3,32 +3,38 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-      let rotten = [];
-  let fresh = 0;
   let rows = grid.length;
   let cols = grid[0].length;
+  let fresh = 0;
+  let rotten = [];
+  let minutes = 0;
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === 2) rotten.push([i, j]);
       if (grid[i][j] === 1) fresh++;
+      if (grid[i][j] === 2) rotten.push([i,j]);
     }
   }
   if (fresh === 0) return 0;
-  let minutes = 0;
-  let directions = [
+  const directions = [
     [1, 0],
-    [-1, 0],
     [0, 1],
+    [-1, 0],
     [0, -1],
   ];
   while (rotten.length && fresh > 0) {
-    const size=rotten.length
+    let size = rotten.length;
     for (let i = 0; i < size; i++) {
       let [r, c] = rotten.shift();
       for (let [dr, dc] of directions) {
-        const nr = r + dr;
-        const nc = c + dc;
-        if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] == 1) {
+        let nr = dr + r;
+        let nc = dc + c;
+        if (
+          nr >= 0 &&
+          nc >= 0 &&
+          nr < rows &&
+          nc < cols &&
+          grid[nr][nc] === 1
+        ) {
           grid[nr][nc] = 2;
           fresh--;
           rotten.push([nr, nc]);
