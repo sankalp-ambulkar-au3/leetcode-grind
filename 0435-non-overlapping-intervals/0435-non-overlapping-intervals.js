8cb37828bf5a8eb15d75d336b806f02f0fe2d443
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-04-13 16:07:47 +0530

    Time: 72 ms (79.09%), Space: 86.1 MB (68.22%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-      if (!intervals || intervals.length === 0) return [];
-  let count=0
+  if (!intervals || !intervals.length) return [];
+  let count = 0;
   intervals.sort((a, b) => a[1] - b[1]);
-  let currentEnd =intervals[0][1]
-  for(let i=1;i<intervals.length;i++)
-  {
-    if(intervals[i][0]<currentEnd)
-    {
-        count++
-    }
-    else{
-        currentEnd=intervals[i][1]
-    }
+  let lastEndTime = intervals[0][1];
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] < lastEndTime) count++;
+    else lastEndTime = intervals[i][1];
   }
-  return count
+  return count;
 };
