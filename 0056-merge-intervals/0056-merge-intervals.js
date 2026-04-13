032a551461c032bd085f9ac6c187b98c9168db79
Sankalp Ambulkar <50396777+sankalp-ambulkar-au3@users.noreply.github.com>
2026-04-13 15:49:22 +0530

    Time: 18 ms (12.83%), Space: 67.3 MB (9.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -10,22 +10,18 @@
 // Push the last current in the result
 
 var merge = function (intervals) {
-  if(!intervals || !intervals.length)
-    return []
-  let res=[]
-  intervals.sort((a,b)=>a[0]-b[0])
-  let current=[...intervals[0]]
-  for(let i=1;i<intervals.length;i++)
-  {
-    let [start,end]=intervals[i]
-    if(start>current[1])
-    {
-      res.push(current)
-      current=[start,end]
-    }
-    else
-    current[1]=Math.max(end,current[1])
+  if (!intervals || !intervals.length) return [];
+  let res = [];
+  intervals.sort((a, b) => a[0] - b[0]);
+  let current = [...intervals[0]];
+
+  for (let i = 1; i < intervals.length; i++) {
+    let [start, end] = intervals[i];
+    if (start > current[1]) {
+      res.push(current);
+      current = [start, end];
+    } else current[1] = Math.max(end, current[1]);
   }
-  res.push(current)
-  return res
+  res.push(current);
+  return res;
 };
